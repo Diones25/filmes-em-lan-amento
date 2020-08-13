b31e7d31b78ab654c4a3e14690fefa2b3105eb4e
Diones Pereira Alves <pereiradiones987@gmail.com>
2020-08-13 08:30:27 -0300

    subindo projeto para o repositório

---
+++ README.md
@@ -0,0 +1,3 @@
+#Sistema de de busca de filmes em lançamento.
+#O projeto usa uma API do site: https://www.themoviedb.org/, para realizar as buscas.
+#Os arquivos de script de exemplo estão todos no projeto.
---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+$(function () {
+    let header = {
+        'Content-Type': 'application/json;charset=utf-8'
+    };
+    $.ajax({
+        headers: header,
+        url: 'https://api.themoviedb.org/3/movie/upcoming?api_key=5c00458ad31f79492cdae66271d31da4&language=pt-BR&page=1&region=br',
+        /*api_key=5c00458ad31f79492cdae66271d31da4*/
+        type: 'GET',
+        dataType: 'json',
+        beforeSend: function () {
+            $('.filmes').html('<div class="col-md-12">Carregando...</div>')
+        },
+        success: function (json) {
+            console.log(json.results);
+            let html = '';
+            for (let i in json.results) {
+                if (json.results[i].poster_path) {
+                    html += `<div class="col-6 col-sm-6 col-md-3 d-flex"><div class="filme text-center"><img class="img-fluid img-thumbnail" src="https://image.tmdb.org/t/p/w185_and_h278_bestv2${json.results[i].poster_path}"/><h5>${json.results[i].title}</h5></div></div>`
+                }
+            }
+            $('.filmes').html(html);
+        }
+    })
+})
